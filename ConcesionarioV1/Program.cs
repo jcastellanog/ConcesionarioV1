7c719885f7095b4381ff681fc9b8c9fadf4710f2
jcastellanog <jesus.castellano@hotmail.com>
2024-01-29 12:04:09 +0100

    Agregar archivos de proyecto.

---
+++ ConcesionarioV1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConcesionarioV1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // CONTROLADOR PRINCIPAL DE LA APLICACIÓN
+
+            // MENÚ DE ACCIONES
+
+            // PROCESO DE LAS ACCIONES
+
+        }
+    }
+}
