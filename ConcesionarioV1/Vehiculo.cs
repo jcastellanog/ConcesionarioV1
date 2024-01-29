542651012f060a2f0f3e8c472740f761ee8542b2
jcastellanog <jesus.castellano@hotmail.com>
2024-01-29 12:37:09 +0100

    Clase Vehiculo: Miembros privados, propiedades y constructores

--- ConcesionarioV1/Vehiculo.cs
+++ ConcesionarioV1/Vehiculo.cs
@@ -11,10 +11,32 @@ namespace ConcesionarioV1
         // CONSTANTES
 
         // MIEMBROS PRIVADOS
+        private string _marca;
+        private string _modelo;
 
         // CONSTRUCTORES
+        public Vehiculo(string marca, string modelo)
+        {
+            Marca = marca;
+            Modelo = modelo;
+        }
 
         // PROPIEDADES
+        public string Marca
+        {
+            get { return _marca; }
+            set { 
+                // TODO: Proporcionar seguridad y protección al miembro - _marca
+                _marca = value; }
+        }
+
+        public string Modelo
+        {
+            get { return _modelo; }
+            set {
+                // TODO: Proporcionar seguridad y protección al miembro - _modelo
+                _modelo = value; }
+        }
 
         // MÉTODOS PÚBLICOS
 
